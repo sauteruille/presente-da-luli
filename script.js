d93b98bac94137e7a73f4671bc5d6916f13d9021
sauteruille <141425637+sauteruille@users.noreply.github.com>
2026-02-22 18:32:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let estado = 0;
+let botao = document.getElementById("botao");
+let imagem = document.getElementById("imagem");
+let legenda = document.getElementById("texto");
+let h2 = document.getElementById("h2");
+let h1 = document.getElementById("h1");
+let  p = "Vejam essa menina linda!!";
+
+botao.addEventListener ("click", function abrirPresente() {
+    if (estado == 0) {
+        imagem.src = "imagem.png";
+        imagem.style.display = "block";
+        h2.style.display = "block";
+        h1.style.display = "block";
+        legenda.style.display = "none";
+        imagem.style.width ="20%";
+        botao.textContent = "Abrir Presente...";
+        estado = 1;
+    } else if (estado == 1) {
+        imagem.src = "ellysfofa.png";
+        imagem.style.width = "30%";
+        legenda.textContent = p;
+        legenda.style.display = "block";
+        h2.style.display = "none";
+        h1.style.display = "none";
+        botao.textContent = "Parece que tem mais alguma coisa! Vamos descobrir!";
+        estado = 2;
+    } else {
+        imagem.src = "ellysloca.png";
+        imagem.style.width = "40%";
+        legenda.style.display = "none";
+        botao.textContent = "Fechar caixa.";
+        estado = 0;
+    }
+});
+
